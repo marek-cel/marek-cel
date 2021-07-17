be8254aa6a8ed817b361e8a75fd9031862b7e3c7
marek-cel <46421808+marek-cel@users.noreply.github.com>
2021-07-17 17:40:44 +0200

    python test code - tic tac toe

--- python/test/15_tic_tac_toe/15_tic_tac_toe.py
+++ python/test/15_tic_tac_toe/15_tic_tac_toe.py
@@ -3,6 +3,43 @@
 ################################################################################
 
 
+X = "X"
+O = "O"
+EMPTY = " "
+TIE = "TIE"
+NUM_FIELDS = 9
+
+################################################################################
+
+
+def main():
+    print_instructions()
+
+    board = [i for i in range(0, NUM_FIELDS)]
+    print_board(board)
+
+    board = [" " for i in range(0, NUM_FIELDS)]
+
+    winner = None
+    token = X
+
+    while not winner:
+        if token == X:
+            human_move(board)
+        else:
+            computer_move(board)
+
+        token = switch_player(token)
+
+        print_board(board)
+        winner = get_winner(board)
+
+        if len(legal_moves(board)) == 0:
+            break
+
+################################################################################
+
+
 def print_instructions():
     print("This is a tic-tac-toe game")
 
@@ -23,55 +60,72 @@ def print_board(b):
 def pick_field(b):
     field_id = int(input("Pick field "))
 
-    if field_id in range(0, 9, 1):
-        if b[field_id] == " ":
-            b[field_id] = "X"
-            return True, b
+    if field_id in legal_moves(b):
+        b[field_id] = X
+        return True
 
-    return False, b
-
-################################################################################
-
-
-def computer_move(b):
-    for i in range(0, len(b), 1):
-        if b[i] == " ":
-            b[i] = "O"
-            break
-
-################################################################################
-
-
-def is_finished():
     return False
 
 ################################################################################
 
 
-print_instructions()
+def human_move(b):
+    result = False
+    while not result:
+        result = pick_field(b)
 
-board = [i for i in range(0, 9, 1)]
-print_board(board)
+################################################################################
 
-board = [" " for i in range(0, 9, 1)]
 
-finished = False
-player = 0
+def computer_move(b):
+    for i in legal_moves(b):
+        b[i] = O
+        break
 
-while not finished:
-    if player == 0:
-        result = False
-        while not result:
-            result, board = pick_field(board)
+################################################################################
+
+
+def switch_player(t):
+    if t == X:
+        return O
     else:
-        computer_move(board)
+        return X
 
-    if player == 0:
-        player = 1
-    else:
-        player = 0
+################################################################################
 
-    print_board(board)
-    finished = is_finished()
+
+def get_winner(b):
+    WAYS_TO_WIN = (
+        (0, 1, 2),
+        (3, 4, 5),
+        (6, 7, 8),
+        (0, 3, 6),
+        (1, 4, 7),
+        (2, 5, 8),
+        (0, 4, 8),
+        (2, 4, 6)
+    )
+
+    for seq in WAYS_TO_WIN:
+        if b[seq[0]] == b[seq[1]] == b[seq[2]] != EMPTY:
+            return b[seq[0]]
+
+    return None
+
+################################################################################
+
+
+def legal_moves(b):
+    lm = []
+    for i in range(0, NUM_FIELDS):
+        if b[i] == " ":
+            lm.append(i)
+
+    return lm
+
+################################################################################
+
+
+main()
 
 ################################################################################
