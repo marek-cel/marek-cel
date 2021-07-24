87d937d68c8a61646a393e81de86a38507f122cc
marek-cel <46421808+marek-cel@users.noreply.github.com>
2021-07-24 08:00:34 +0200

    python PyQt test codes

---
+++ python/PyQt/01_hello_world/01_hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+################################################################################
+
+import sys
+
+from PySide2.QtWidgets import QApplication, QLabel
+
+
+app = QApplication(sys.argv)
+
+#label = QLabel("Hello World!")
+
+label = QLabel("<font color=red size=40>Hello World!</font>")
+
+label.show()
+
+app.exec_()
