16844850597f2f0c06c60064c5d71af12b3d47a0
Marek Cel <marek.cel@gmail.com>
2022-03-19 21:38:28 +0100

    octave test codes

---
+++ octave/test/ex37_menu.m
@@ -0,0 +1,7 @@
+clear all
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+option = menu('Menu Title', 'Option 1', 'Option 2', 'Option 3');
+
+fprintf('Selected option is %i\n', option);
