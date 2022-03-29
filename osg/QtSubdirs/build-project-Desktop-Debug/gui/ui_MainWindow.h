546261afc808b40737fad4f1b62222823e01598b
Marek Cel <marek.cel@gmail.com>
2022-03-29 16:35:08 +0200

    OSG test codes

---
+++ osg/QtSubdirs/build-project-Desktop-Debug/gui/ui_MainWindow.h
@@ -0,0 +1,66 @@
+/********************************************************************************
+** Form generated from reading UI file 'MainWindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.8
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QLocale>
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QWidget>
+#include "WidgetCGI.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGridLayout *gridLayout;
+    WidgetCGI *widgetCGI;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(1024, 768);
+        MainWindow->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
+        MainWindow->setDockOptions(QMainWindow::AllowNestedDocks|QMainWindow::AllowTabbedDocks|QMainWindow::AnimatedDocks);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        gridLayout = new QGridLayout(centralwidget);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        widgetCGI = new WidgetCGI(centralwidget);
+        widgetCGI->setObjectName(QString::fromUtf8("widgetCGI"));
+
+        gridLayout->addWidget(widgetCGI, 0, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralwidget);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "OverlayNode", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ osg/QtSubdirs/src/gui/gui.pri
@@ -0,0 +1,15 @@
+HEADERS += \
+    $$PWD/GraphicsWindowQt.h \
+    $$PWD/KeyMap.h \
+    $$PWD/MainWindow.h \
+    $$PWD/WidgetCGI.h
+
+SOURCES += \
+    $$PWD/GraphicsWindowQt.cpp \
+    $$PWD/KeyMap.cpp \
+    $$PWD/MainWindow.cpp \
+    $$PWD/WidgetCGI.cpp \
+    $$PWD/main.cpp
+
+FORMS += \
+    $$PWD/MainWindow.ui
