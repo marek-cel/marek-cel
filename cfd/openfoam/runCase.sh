546b625b8f538a2ea07ddeff8f0bfa422ce06b06
Marek Cel <marek.cel@gmail.com>
2022-07-19 18:36:55 +0200

    OpenFOAM - blockMesh basic example

---
+++ cfd/openfoam/runCase.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+################################################################################
+
+CURRENT_DIR=$(pwd)
+
+rm -r $FOAM_RUN/$1
+cp -r $1 $FOAM_RUN/
+cd $FOAM_RUN/$1
+./runAll.sh
+cd $CURRENT_DIR
