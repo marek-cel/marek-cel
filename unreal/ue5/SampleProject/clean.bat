59093584c21dfd7950a554efae5c2a1fadbb81be
Marek Cel <marek.cel@gmail.com>
2023-01-31 17:56:52 +0100

    Unreal Engine - Sample Project

---
+++ unreal/ue5/SampleProject/clean.bat
@@ -0,0 +1,4 @@
+rmdir /s /q Binaries
+rmdir /s /q Intermediate
+rmdir /s /q DerivedDataCache
+rmdir /s /q Saved
---
+++ unreal/ue5/SampleProject/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -r -f Binaries
+rm -r -f Intermediate
+rm -r -f DerivedDataCache
+rm -r -f Saved
