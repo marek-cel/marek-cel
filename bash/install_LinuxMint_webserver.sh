604beb0e97743e63c32b4a8cb2384a4a6deb5e91
Marek Cel <marek.cel@gmail.com>
2023-11-22 19:45:53 +0100

    Add more options to web server Linux installation scripts

--- bash/install_LinuxMint_webserver.sh
+++ bash/install_LinuxMint_webserver.sh
@@ -13,13 +13,62 @@ function installWebServer()
     then
         printGreen "Installing WEB SERVER ..."
 
-        sudo apt install -y \
-            apache2 \
-            mysql-client \
-            mysql-server \
-            php \
-            php-intl \
-            phpmyadmin
+        readBold "Do you want to install APACHE2? y or n"
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            printGreen "Installing APACHE2 ..."
+
+            sudo apt install -y \
+                apache2
+        fi
+
+        readBold "Do you want to install NGINX? y or n"
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            printGreen "Installing NGINX ..."
+
+            sudo apt install -y \
+                nginx
+        fi
+
+        readBold "Do you want to install MYSQL? y or n"
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            printGreen "Installing MYSQL ..."
+
+            sudo apt install -y \
+                mysql-client \
+                mysql-server
+        fi
+
+        readBold "Do you want to install MARIADB? y or n"
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            printGreen "Installing MARIADB ..."
+
+            sudo apt install -y \
+                mariadb-client \
+                mariadb-server
+        fi
+
+        readBold "Do you want to install PHP? y or n"
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            printGreen "Installing PHP ..."
+
+            sudo apt install -y \
+                php \
+                php-intl
+        fi
+
+        readBold "Do you want to install PHPMYADMIN? y or n"
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            printGreen "Installing PHPMYADMIN ..."
+
+            sudo apt install -y \
+                phpmyadmin
+        fi
     fi
 }
 
