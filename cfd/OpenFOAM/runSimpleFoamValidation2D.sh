cc342f0ab49dc0369818556918350b04b855f31d
Marek Cel <marek.cel@gmail.com>
2024-02-01 05:29:39 +0100

    Update CFD examples

--- cfd/OpenFOAM/runSimpleFoamValidation2D.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# ./runSequanceAoA.sh incompressible/simpleFoam/03_Validation_2D 0 20 2
-# ./runSequanceAoA.sh incompressible/simpleFoam/03_Validation_2D 25 90 5
-
-./runSequanceAoA.sh incompressible/simpleFoam/03_Validation_2D 0 30 10
-./runSequanceAoA.sh incompressible/simpleFoam/03_Validation_2D 30 90 10
---
+++ cfd/OpenFOAM/runSimpleFoamValidationNACA0012.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./runSequanceAoA.sh incompressible/simpleFoam/04_Validation_NACA0012 0 20 4
