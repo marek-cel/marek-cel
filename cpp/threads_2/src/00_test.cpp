20e16ddb87f80a3d1a92255f8e2d1d688e3ce761
Marek Cel <marek.cel@gmail.com>
2024-08-23 15:39:19 +0200

    Update C++ examples

---
+++ cpp/threads_2/src/00_test.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#ifdef WIN32
+#   include <windows.h>
+#else
+#   include <unistd.h>
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+
+void fun()
+{
+    for ( int i = 0; i < 10; ++i )
+    {
+        std::this_thread::sleep_for( std::chrono::milliseconds(500) );
+        std::cout << "Loop " << i << std::endl;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    std::cout << __FILE__ << std::endl;
+    std::cout << __cplusplus << std::endl;
+    std::cout << std::endl;
+
+    // constructs new thread object
+    std::thread t1( fun );
+    std::thread t2( fun );
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
