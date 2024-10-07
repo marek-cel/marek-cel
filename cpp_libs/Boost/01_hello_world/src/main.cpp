a33ecf39d9f1ba2a3e8cfff3a425a953dae3ba0a
Marek Cel <marek.cel@gmail.com>
2024-10-07 15:51:04 +0200

    Update Boost and SDL examples

--- cpp_libs/Boost/01_hello_world/src/main.cpp
+++ cpp_libs/Boost/01_hello_world/src/main.cpp
@@ -1,28 +1,12 @@
+#include <boost/function.hpp>
 #include <iostream>
 
-#include <boost/signals2/signal.hpp>
+void hello() {
+    std::cout << "Hello, world!" << std::endl;
+}
 
-struct HelloWorld
-{
-	void operator()() const
-	{
-		std::cout << "Hello, World!" << std::endl;
-	}
-};
-
-int main()
-{
-	std::cout << "Hello, World!" << std::endl;
-
-	// Signal with no arguments and a void return value
-	boost::signals2::signal<void ()> sig;
-
-	// Connect a HelloWorld slot
-	HelloWorld hello;
-	sig.connect(hello);
-
-	// Call all of the slots
-	sig();
-
-	return 0;
+int main() {
+    boost::function<void()> f = &hello;
+    f();
+    return 0;
 }
