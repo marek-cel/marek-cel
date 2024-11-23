586e7e54af8eb7e88f04c543c4202971f3aaebdf
Marek Cel <marek.cel@gmail.com>
2024-11-23 11:00:28 +0100

    Update units example

--- cpp_libs/Physics/units/src/main.cpp
+++ cpp_libs/Physics/units/src/main.cpp
@@ -17,6 +17,14 @@ int main(int argc, char* argv[])
     units::force::newton_t f = 1.0_N;
     units::torque::newton_meter_t t = f * l;
     std::cout << "t = " << t << std::endl;
+    std::cout << "t = " << static_cast<double>(t) << std::endl;
+
+    units::angle::radian_t a = 1.0_rad * M_PI;
+    units::angle::degree_t d = a;
+    std::cout << "d = " << d << std::endl;
+    std::cout << "d = " << d() << std::endl;
+    auto str = units::angle::to_string(d);
+    std::cout << "d = " << str << std::endl;
 
     return 0;
 }
