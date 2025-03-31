ddd0c93dd35096992f1db716fa680ba608adc66f
Marek Cel <marek.cel@gmail.com>
2025-03-31 20:46:53 +0200

    Update C++ Boost examples

--- cpp_libs/Boost/02_logger/src/main.cpp
+++ cpp_libs/Boost/02_logger/src/main.cpp
@@ -3,8 +3,10 @@
 #define BOOST_LOG_DYN_LINK 1
 
 #include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/file.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -15,8 +17,11 @@ void init()
     boost::log::add_file_log(
         boost::log::keywords::file_name = "sample.log",
         boost::log::keywords::target_file_name = "sample.log",
-        boost::log::keywords::format = "[%TimeStamp%]: %Message%"
+        boost::log::keywords::auto_flush = true,
+        boost::log::keywords::format = "[%TimeStamp%] %Message%"
     );
+
+    boost::log::add_common_attributes();
 
     //boost::log::add_file_log( "sample.log" );
 
@@ -33,9 +38,8 @@ int main()
     std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl;
 
     init();
+    BOOST_LOG_TRIVIAL(error) << "Hello, world!";
 
-    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-    BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
 
     std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl;
 
