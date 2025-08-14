47e74a412350cad5a40797adbe70a1efc7197e73
Marek Cel <marek.cel@gmail.com>
2025-08-14 19:11:05 +0200

    Add GLSL examples

---
+++ glsl/110/11_Clouds/src/main.cpp
@@ -0,0 +1,202 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <osg/ArgumentParser>
+#include <osg/Geode>
+#include <osg/Group>
+#include <osg/Light>
+#include <osg/LightSource>
+#include <osg/PositionAttitudeTransform>
+#include <osg/ShapeDrawable>
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+
+#include <osgGA/TrackballManipulator>
+#include <osgGA/FlightManipulator>
+#include <osgGA/DriveManipulator>
+#include <osgGA/KeySwitchMatrixManipulator>
+#include <osgGA/StateSetManipulator>
+#include <osgGA/AnimationPathManipulator>
+#include <osgGA/TerrainManipulator>
+#include <osgGA/SphericalManipulator>
+
+const char* vertCode = R"(
+#version 110
+
+void main()
+{
+    gl_Position = ftransform();
+}
+)";
+
+const char* fragCode = R"(
+#version 110
+
+void main()
+{
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+)";
+
+void setupCameraManipulators(osgViewer::Viewer* viewer, osg::ArgumentParser* arguments)
+{
+    osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManip = new osgGA::KeySwitchMatrixManipulator();
+
+    keyswitchManip->addMatrixManipulator( '1', "Trackball"   , new osgGA::TrackballManipulator()   );
+    keyswitchManip->addMatrixManipulator( '2', "Flight"      , new osgGA::FlightManipulator()      );
+    keyswitchManip->addMatrixManipulator( '3', "Drive"       , new osgGA::DriveManipulator()       );
+    keyswitchManip->addMatrixManipulator( '4', "Terrain"     , new osgGA::TerrainManipulator()     );
+    keyswitchManip->addMatrixManipulator( '5', "Orbit"       , new osgGA::OrbitManipulator()       );
+    keyswitchManip->addMatrixManipulator( '6', "FirstPerson" , new osgGA::FirstPersonManipulator() );
+    keyswitchManip->addMatrixManipulator( '7', "Spherical"   , new osgGA::SphericalManipulator()   );
+
+    std::string pathfile;
+    double animationSpeed = 1.0;
+    while(arguments->read("--speed",animationSpeed) ) {}
+    char keyForAnimationPath = '8';
+    while (arguments->read("-p",pathfile))
+    {
+        osgGA::AnimationPathManipulator* apm = new osgGA::AnimationPathManipulator(pathfile);
+        if (apm || !apm->valid())
+        {
+            apm->setTimeScale(animationSpeed);
+
+            unsigned int num = keyswitchManip->getNumMatrixManipulators();
+            keyswitchManip->addMatrixManipulator(keyForAnimationPath, "Path", apm);
+            keyswitchManip->selectMatrixManipulator(num);
+            ++keyForAnimationPath;
+        }
+    }
+
+    viewer->setCameraManipulator(keyswitchManip.get());
+}
+
+void setupEventHandlers(osgViewer::Viewer* viewer, osg::ArgumentParser* arguments)
+{
+    // add the state manipulator
+    viewer->addEventHandler(new osgGA::StateSetManipulator(viewer->getCamera()->getOrCreateStateSet()));
+
+    // add the thread model handler
+    viewer->addEventHandler(new osgViewer::ThreadingHandler);
+
+    // add the window size toggle handler
+    viewer->addEventHandler(new osgViewer::WindowSizeHandler);
+
+    // add the stats handler
+    viewer->addEventHandler(new osgViewer::StatsHandler);
+
+    // add the help handler
+    viewer->addEventHandler(new osgViewer::HelpHandler(arguments->getApplicationUsage()));
+
+    // add the record camera path handler
+    viewer->addEventHandler(new osgViewer::RecordCameraPathHandler);
+
+    // add the LOD Scale handler
+    viewer->addEventHandler(new osgViewer::LODScaleHandler);
+
+    // add the screen capture handler
+    viewer->addEventHandler(new osgViewer::ScreenCaptureHandler);
+}
+
+osg::PositionAttitudeTransform* createLight(osg::Group* root)
+{
+    osg::ref_ptr<osg::PositionAttitudeTransform> lightPat = new osg::PositionAttitudeTransform();
+    root->addChild(lightPat.get());
+
+    osg::ref_ptr<osg::LightSource> lightSource = new osg::LightSource();
+    lightPat->addChild(lightSource.get());
+
+    osg::ref_ptr<osg::Light> lightSun = new osg::Light();
+
+    lightSun->setLightNum(0);
+
+    osg::Vec4 color(1.0, 1.0, 1.0, 1.0);
+    osg::Vec3d position(6.0, -2.0, 4.0);
+
+    lightSun->setPosition(osg::Vec4d(position, 1.0));
+    lightPat->setPosition(position);
+
+    lightSun->setAmbient  ( color );
+    lightSun->setDiffuse  ( color );
+    lightSun->setSpecular ( color );
+
+    lightSun->setConstantAttenuation(1.0);
+
+    lightSource->setLight(lightSun.get());
+
+    lightSource->setLocalStateSetModes(osg::StateAttribute::ON);
+    lightSource->setStateSetModes(*root->getOrCreateStateSet(), osg::StateAttribute::ON);
+
+    // add sphere representing light source
+    osg::ref_ptr<osg::Sphere> lightSphere = new osg::Sphere(position, 0.1);
+    osg::ref_ptr<osg::ShapeDrawable> lightDrawable = new osg::ShapeDrawable(lightSphere.get());
+    lightDrawable->setColor(osg::Vec4(1.0, 1.0, 0.0, 1.0)); // yellow color
+    lightPat->addChild(lightDrawable.get());
+
+    return lightPat.release();
+}
+
+osg::Group* createScene()
+{
+    osg::ref_ptr<osg::Group> root = new osg::Group();
+
+    osg::ref_ptr<osg::StateSet> stateSet = root->getOrCreateStateSet();
+    stateSet->setMode( GL_RESCALE_NORMAL , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_LIGHTING       , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_LIGHT0         , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_BLEND          , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_ALPHA_TEST     , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_DEPTH_TEST     , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_DITHER         , osg::StateAttribute::OFF );
+
+    createLight(root.get());
+
+    osg::ref_ptr<osg::Group> cloud = new osg::Group();
+    root->addChild(cloud.get());
+
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+    cloud->addChild(geode.get());
+    osg::ref_ptr<osg::Box> box = new osg::Box(osg::Vec3f(), 3.0, 4.0, 5.0);
+    osg::ref_ptr<osg::ShapeDrawable> shape = new osg::ShapeDrawable(box.get());
+    geode->addDrawable(shape.get());
+
+    osg::ref_ptr<osg::Geode> geode2 = new osg::Geode();
+    cloud->addChild(geode2.get());
+    osg::ref_ptr<osg::Box> box2 = new osg::Box(osg::Vec3f(2.0, 3.0, 0.0), 4.0, 5.0, 2.0);
+    osg::ref_ptr<osg::ShapeDrawable> shape2 = new osg::ShapeDrawable(box2.get());
+    geode2->addDrawable(shape2.get());
+
+    osg::ref_ptr<osg::Geode> geode3 = new osg::Geode();
+    cloud->addChild(geode3.get());
+    osg::ref_ptr<osg::Box> box3 = new osg::Box(osg::Vec3f(0.0, -1.0, 3.0), 2.0, 3.0, 4.0);
+    osg::ref_ptr<osg::ShapeDrawable> shape3 = new osg::ShapeDrawable(box3.get());
+    geode3->addDrawable(shape3.get());
+
+    osg::ref_ptr<osg::StateSet> cloudStateSet = cloud->getOrCreateStateSet();
+
+    osg::ref_ptr<osg::Program> program = new osg::Program;
+    program->addShader(new osg::Shader(osg::Shader::VERTEX   , vertCode));
+    program->addShader(new osg::Shader(osg::Shader::FRAGMENT , fragCode));
+    cloudStateSet->setAttributeAndModes(program.get());
+
+    return root.release();
+}
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "");
+
+    osg::ArgumentParser arguments(&argc, argv);
+
+    osgViewer::Viewer viewer(arguments);
+
+    setupCameraManipulators(&viewer, &arguments);
+    setupEventHandlers(&viewer, &arguments);
+
+    viewer.setUpViewInWindow(0, 0, 800, 600);
+    viewer.setSceneData(createScene());
+
+    return viewer.run();
+}
