a2b83aac3d5c1aa929cd420bdced5eb3929837ee
Marek Cel <marek.cel@gmail.com>
2025-08-17 09:48:52 +0200

    Update GLSL examples

---
+++ glsl/110/13_spherical_points/src/main.cpp
@@ -0,0 +1,164 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <osg/ArgumentParser>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/Group>
+#include <osg/Light>
+#include <osg/LightSource>
+#include <osg/PositionAttitudeTransform>
+#include <osg/ShapeDrawable>
+#include <osg/Texture2D>
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+
+#include <osgGA/TrackballManipulator>
+#include <osgGA/StateSetManipulator>
+
+// Vertex Shader
+const char* vertCode = R"(
+#version 120
+
+void main()
+{
+    // TODO
+}
+)";
+
+// Fragment Shader
+const char* fragCode = R"(
+#version 120
+
+void main()
+{
+    // TODO
+}
+)";
+
+void setupEventHandlers(osgViewer::Viewer* viewer, osg::ArgumentParser* arguments)
+{
+    // add the state manipulator
+    viewer->addEventHandler(new osgGA::StateSetManipulator(viewer->getCamera()->getOrCreateStateSet()));
+
+    // add the thread model handler
+    viewer->addEventHandler(new osgViewer::ThreadingHandler);
+
+    // add the window size toggle handler
+    viewer->addEventHandler(new osgViewer::WindowSizeHandler);
+
+    // add the stats handler
+    viewer->addEventHandler(new osgViewer::StatsHandler);
+
+    // add the help handler
+    viewer->addEventHandler(new osgViewer::HelpHandler(arguments->getApplicationUsage()));
+
+    // add the record camera path handler
+    viewer->addEventHandler(new osgViewer::RecordCameraPathHandler);
+
+    // add the LOD Scale handler
+    viewer->addEventHandler(new osgViewer::LODScaleHandler);
+
+    // add the screen capture handler
+    viewer->addEventHandler(new osgViewer::ScreenCaptureHandler);
+}
+
+osg::PositionAttitudeTransform* createLight(osg::Group* root)
+{
+    osg::ref_ptr<osg::PositionAttitudeTransform> lightPat = new osg::PositionAttitudeTransform();
+    root->addChild(lightPat.get());
+
+    osg::ref_ptr<osg::LightSource> lightSource = new osg::LightSource();
+    lightPat->addChild(lightSource.get());
+
+    osg::ref_ptr<osg::Light> lightSun = new osg::Light();
+
+    lightSun->setLightNum(0);
+
+    osg::Vec4 color(1.0, 1.0, 1.0, 1.0);
+    osg::Vec3d position(6.0, -2.0, 4.0);
+
+    lightSun->setPosition(osg::Vec4d(position, 1.0));
+    lightPat->setPosition(position);
+
+    lightSun->setAmbient  ( color );
+    lightSun->setDiffuse  ( color );
+    lightSun->setSpecular ( color );
+
+    lightSun->setConstantAttenuation(1.0);
+
+    lightSource->setLight(lightSun.get());
+
+    lightSource->setLocalStateSetModes(osg::StateAttribute::ON);
+    lightSource->setStateSetModes(*root->getOrCreateStateSet(), osg::StateAttribute::ON);
+
+    // add sphere representing light source
+    osg::ref_ptr<osg::Sphere> lightSphere = new osg::Sphere(position, 0.1);
+    osg::ref_ptr<osg::ShapeDrawable> lightDrawable = new osg::ShapeDrawable(lightSphere.get());
+    lightDrawable->setColor(osg::Vec4(1.0, 1.0, 0.0, 1.0)); // yellow color
+    lightPat->addChild(lightDrawable.get());
+
+    return lightPat.release();
+}
+
+osg::Group* createScene()
+{
+    osg::ref_ptr<osg::Group> root = new osg::Group();
+
+    osg::ref_ptr<osg::StateSet> stateSet = root->getOrCreateStateSet();
+    stateSet->setMode( GL_RESCALE_NORMAL , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_LIGHTING       , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_LIGHT0         , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_BLEND          , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_ALPHA_TEST     , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_DEPTH_TEST     , osg::StateAttribute::ON  );
+    stateSet->setMode( GL_DITHER         , osg::StateAttribute::OFF );
+
+    createLight(root.get());
+
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+    root->addChild(geode.get());
+
+    osg::ref_ptr<osg::Geometry> geom = new osg::Geometry();
+    geode->addDrawable(geom.get());
+
+    osg::ref_ptr<osg::Vec3Array> v = new osg::Vec3Array();  // vertices
+    osg::ref_ptr<osg::Vec4Array> c = new osg::Vec4Array();  // colors
+
+    for ( uint32_t i = 0; i < 10; ++i )
+    {
+        v->push_back(osg::Vec3(i, 0.0f, 0.0f));  // placeholder for water points
+    }
+
+    c->push_back(osg::Vec4(0.3f, 0.7f, 1.0f, 1.0f));  // blue color for water
+
+    geom->setVertexArray(v.get());
+    geom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::POINTS, 0, v->size()));
+    geom->setColorArray(c.get());
+    geom->setColorBinding(osg::Geometry::BIND_OVERALL);
+
+    osg::ref_ptr<osg::StateSet> geodeStateSet = geode->getOrCreateStateSet();
+
+    osg::ref_ptr<osg::Program> program = new osg::Program;
+    // program->addShader(new osg::Shader(osg::Shader::VERTEX   , vertCode));
+    program->addShader(new osg::Shader(osg::Shader::FRAGMENT , fragCode));
+    geodeStateSet->setAttributeAndModes(program.get());
+
+    return root.release();
+}
+
+int main(int argc, char* argv[])
+{
+    osg::ArgumentParser arguments(&argc, argv);
+    osgViewer::Viewer viewer(arguments);
+
+    viewer.setCameraManipulator(new osgGA::TrackballManipulator());
+    viewer.addEventHandler(new osgGA::StateSetManipulator(viewer.getCamera()->getOrCreateStateSet()));
+    viewer.addEventHandler(new osgViewer::StatsHandler());
+    viewer.setUpViewInWindow(0, 0, 800, 600);
+    viewer.setSceneData(createScene());
+
+    return viewer.run();
+}
