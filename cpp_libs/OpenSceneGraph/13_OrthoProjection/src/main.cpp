36a7a6e1fd1c85b6d91cec1128587902c813cbcc
Marek Cel <marek.cel@gmail.com>
2025-10-24 18:26:57 +0200

    Update OpenSceneGraph examples

--- cpp_libs/OpenSceneGraph/13_OrthoProjection/src/main.cpp
+++ cpp_libs/OpenSceneGraph/13_OrthoProjection/src/main.cpp
@@ -3,174 +3,13 @@
 #include <osg/Geode>
 #include <osg/ShapeDrawable>
 
-#include <osgGA/StandardManipulator>
 #include <osgGA/StateSetManipulator>
 
 #include <osgViewer/Viewer>
 #include <osgViewer/ViewerEventHandlers>
 
-class ManipulatorOrtho : public osgGA::StandardManipulator
-{
-public:
-
-    ManipulatorOrtho()
-        : osgGA::StandardManipulator()
-    {}
-
-    void setTransformation(const osg::Vec3d& eye, const osg::Quat& rotation)
-    {
-        // TODO
-    }
-
-
-    void setTransformation(const osg::Vec3d& eye, const osg::Vec3d& center, const osg::Vec3d& up)
-    {
-        // TODO
-    }
-
-
-    void getTransformation(osg::Vec3d& eye, osg::Quat& rotation) const
-    {
-        // TODO
-    }
-
-
-    void getTransformation(osg::Vec3d& eye, osg::Vec3d& center, osg::Vec3d& up) const
-    {
-        // TODO
-    }
-
-
-    void setByMatrix(const osg::Matrixd& matrix)
-    {
-        // TODO
-    }
-
-
-    void setByInverseMatrix(const osg::Matrixd& matrix)
-    {
-        // TODO
-    }
-
-    osg::Matrixd getMatrix() const
-    {
-        // TODO
-        return osg::Matrixd::identity();
-    }
-
-
-    osg::Matrixd getInverseMatrix() const
-    {
-        // TODO
-        return osg::Matrixd::identity();
-    }
-
-    void updateCamera(osg::Camera& camera) override
-    {
-        osgGA::StandardManipulator::updateCamera(camera);
-
-        double w2h = (double)(camera.getGraphicsContext()->getTraits()->width)
-                / (double)(camera.getGraphicsContext()->getTraits()->height);
-
-        double x_min = -_scale * _fov_y * w2h / 2.0;
-        double x_max =  _scale * _fov_y * w2h / 2.0;
-        double y_min = -_scale * _fov_y / 2.0;
-        double y_max =  _scale * _fov_y / 2.0;
-
-        if ( x_min != _x_min || x_max != _x_max || y_min != _y_min || y_max != _y_max )
-        {
-            _x_min = x_min;
-            _x_max = x_max;
-            _y_min = y_min;
-            _y_max = y_max;
-
-            camera.setProjectionMatrixAsOrtho(_x_min, _x_max, _y_min, _y_max, _z_near, _z_far);
-        }
-    }
-
-    void setFovY(double fov_y)
-    {
-        if ( fov_y > 0.0 )
-        {
-            _fov_y = fov_y;
-        }
-    }
-
-    void setScale(double scale)
-    {
-        _scale = scale;
-
-        if ( _scale > _scale_max ) _scale = _scale_max;
-        if ( _scale < _scale_min ) _scale = _scale_min;
-    }
-
-
-    void setScaleMin(double min)
-    {
-        if ( min >= 0.0 && min < _scale_max )
-        {
-            _scale_min = min;
-        }
-    }
-
-
-    void setScaleMax(double max)
-    {
-        if ( max >= 0.0 && max > _scale_min )
-        {
-            _scale_max = max;
-        }
-    }
-
-    void setWheelZoomFactor(double wheelZoomFactor)
-    {
-        _wheelZoomFactor = wheelZoomFactor;
-    }
-
-protected:
-
-    double _fov_y = 1.0;
-
-    double _x_min = -_fov_y / 2.0;
-    double _x_max =  _fov_y / 2.0;
-    double _y_min = -_fov_y / 2.0;
-    double _y_max =  _fov_y / 2.0;
-
-    double _scale = 1.0;
-    double _scale_min = 0.1;
-    double _scale_max = 100.0;
-
-    double _z_near = 0.001;
-    double _z_far = 1000.0;
-
-    double _wheelZoomFactor = -0.1;
-
-    bool handleMouseWheel(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& us) override
-    {
-        osgGA::GUIEventAdapter::ScrollingMotion sm = ea.getScrollingMotion();
-
-        switch ( sm )
-        {
-        case osgGA::GUIEventAdapter::SCROLL_UP:
-            zoomModel( -_wheelZoomFactor );
-            us.requestRedraw();
-            return true;
-
-        case osgGA::GUIEventAdapter::SCROLL_DOWN:
-            zoomModel(  _wheelZoomFactor );
-            us.requestRedraw();
-            return true;
-        }
-
-        return false;
-    }
-
-    void zoomModel(const float dy)
-    {
-        std::cout << __FILE__ << "(" << __LINE__ << ")" << std::endl;
-        setScale(_scale * (1.0 + dy));
-    }
-};
+constexpr double fov_y = 50.0;
+constexpr double fov_y_2 = fov_y / 2.0;
 
 void setupEventHandlers(osgViewer::Viewer* viewer)
 {
@@ -186,9 +25,7 @@ void setupEventHandlers(osgViewer::Viewer* viewer)
     // add the stats handler
     viewer->addEventHandler(new osgViewer::StatsHandler);
 
-    // set the custom manipulator
-    osg::ref_ptr<ManipulatorOrtho> manipulator = new ManipulatorOrtho();
-    viewer->setCameraManipulator(manipulator.get());
+    // TODO: set the ortho projection camera manipulator
 }
 
 void setupCamera(osgViewer::Viewer* viewer)
@@ -211,9 +48,6 @@ void setupCamera(osgViewer::Viewer* viewer)
 
     double w2h = static_cast<double>(traits->width)
                / static_cast<double>(traits->height);
-
-    constexpr double fov_y = 50.0;
-    constexpr double fov_y_2 = fov_y / 2.0;
 
     camera->setProjectionMatrixAsOrtho(-fov_y_2 * w2h, fov_y_2 * w2h, -fov_y_2, fov_y_2, 0.1, 1000000.0);
 }
